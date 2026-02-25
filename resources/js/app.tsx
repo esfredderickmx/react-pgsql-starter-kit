fe43923356aa6f97f92335ffc69399da2ca12530
Sebastián Montero <esfredderick@gmail.com>
2026-02-24 22:59:33 -0600

    Manejo del tema visual de la aplicación

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,7 +1,9 @@
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '../css/app.css';
+import { initializeTheme } from '@/hooks/use-appearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -11,9 +13,15 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <StrictMode>
+                <App {...props} />
+            </StrictMode>,
+        );
     },
     progress: {
         color: '#4B5563',
     },
 });
+
+initializeTheme();
